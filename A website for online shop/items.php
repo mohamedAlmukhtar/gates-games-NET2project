8f578f3210f73e9e7873607fd115a3d23006e217
KhalifaAwidat <45952315+KhalifaAwidat@users.noreply.github.com>
2019-02-09 10:44:39 -0800

    items page for users

---
+++ A website for online shop/items.php
@@ -0,0 +1,84 @@
+<?php
+	
+	session_start();
+	$pageTitle = 'Show Items';
+	include 'init.php';
+
+	// Check If Get Request item Is Numeric & Get Its Integer Value
+	$itemid = isset($_GET['itemid']) && is_numeric($_GET['itemid']) ? intval($_GET['itemid']) : 0;
+
+	// Select All Data Depend On This ID
+	$stmt = $con->prepare("SELECT 
+								items.*, 
+								categories.Name AS category_name, 
+								users.Username 
+							FROM 
+								items
+							INNER JOIN 
+								categories 
+							ON 
+								categories.ID = items.Cat_ID 
+							INNER JOIN 
+								users 
+							ON 
+								users.UserID = items.Member_ID 
+							WHERE 
+								Item_ID = ?
+							AND 
+								Approve = 1");
+
+	// Execute Query
+	$stmt->execute(array($itemid));
+
+	$count = $stmt->rowCount();
+
+	if ($count > 0) {
+
+	// Fetch The Data
+	$item = $stmt->fetch();
+?>
+<h1 class="text-center"><?php echo $item['Name'] ?></h1>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<img class="img-responsive img-thumbnail center-block" src="img.png" alt="" />
+		</div>
+		<div class="col-md-9 item-info">
+			<h2><?php echo $item['Name'] ?></h2>
+			<p><?php echo $item['Description'] ?></p>
+			<ul class="list-unstyled">
+				<li>
+					<i class="fa fa-calendar fa-fw"></i>
+					<span>Added Date</span> : <?php echo $item['Add_Date'] ?>
+				</li>
+				<li>
+					<i class="fa fa-money fa-fw"></i>
+					<span>Price</span> : <?php echo $item['Price'] ?>
+				</li>
+				<li>
+					<i class="fa fa-building fa-fw"></i>
+					<span>Made In</span> : <?php echo $item['Country_Made'] ?>
+				</li>
+				<li>
+					<i class="fa fa-tags fa-fw"></i>
+					<span>Category</span> : <a href="categories.php?pageid=<?php echo $item['Cat_ID'] ?>"><?php echo $item['category_name'] ?></a>
+				</li>
+				<li>
+					<i class="fa fa-user fa-fw"></i>
+					<span>Added By</span> : <a href="#"><?php echo $item['Username'] ?></a>
+				</li>
+				
+			</ul>
+		</div>
+	</div>
+	<hr class="custom-hr">
+	
+<?php
+	} else {
+		echo '<div class="container">';
+			echo '<div class="alert alert-danger">There\'s no Such ID Or This Item Is Waiting Approval</div>';
+		echo '</div>';
+	}
+	include $tpl . 'footer.php';
+
+?>
